bdc410190f59bb12991a32b7c99c48e7619ad25a
ika1212 <rika.pear06@gmail.comgit config --global user.name ika1212git config --global user.email rika.pear06@gmail.com>
2020-05-05 18:23:58 +0900

    自動送信機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,13 +1,14 @@
 $(function(){
+
   function buildHTML(message){
     if ( message.image ) {
       var html =
-           `<div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.created_at}
+            `<div class="upper-message" data-message-id=${message.id}>
+                <div class="upper-message__user-name">
+                  ${message.user_name}
+                </div>
+                <div class="upper-message__date">
+                  ${message.created_at}
               </div>
             </div>
             <div class="message-box">
@@ -15,12 +16,12 @@ $(function(){
                 ${message.content}
               </p>
             </div>
-            <img src=${message.image} >
-          </div>`
+            <img src=${message.image} >`
         return html;
       } else {
         var html =
-            `<div class="upper-message">
+          `
+            <div class="upper-message" data-message-id=${message.id}>
               <div class="upper-message__user-name">
                 ${message.user_name}
               </div>
@@ -37,7 +38,7 @@ $(function(){
         return html;
       };
     }
-
+    
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -61,4 +62,40 @@ $(function(){
       alert("メッセージ送信に失敗しました");
   });
   })
-})
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.upper-message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.main-chat__message-list').append(insertHTML);
+      $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+
+      }
+
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+});
+
+
+
