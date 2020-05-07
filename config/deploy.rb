6e6e469806fd0d1fa4abec4cf8f847bae668a639
ika1212 <rika.pear06@gmail.comgit config --global user.name ika1212git config --global user.email rika.pear06@gmail.com>
2020-05-07 19:40:29 +0900

    edit_deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'capistrano 3.14.0'
+lock '3.14.0'
 
 # Capistranoのログの表示に利用する
-set :application, '<chat-space>'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:ika1212/chat-space.git'
